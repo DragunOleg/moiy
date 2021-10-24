7e1c672b5e7923042853a8bcc71175b60ec551e5
dragu <dragun.oleg41@gmail.com>
2021-10-24 12:42:59 +0300

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windсows, actions, and settings.
+from lab1 import simplex_main_phase, c, A, x, Jb
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    simplex_main_phase(c, A, None, x, Jb)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
